65eaea10c6d8ce438c997087989f2b5fafa2a2af
Maxime L <maxime30labbe@gmail.com>
2025-09-05 17:40:54 +0200

    fix: fixed deployment

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,4 @@
 import type { Metadata } from "next";
-import { Pacifico } from "next/font/google";
 import localFont from "next/font/local";
 import "./globals.css";
 import picturesData from "@/data/pictures.json";
